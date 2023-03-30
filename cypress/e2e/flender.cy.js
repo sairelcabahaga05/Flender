2f98d497dddbe23a37aa6222232624b1316bd981
sairelcabahaga05 <scabahaga@ssct.edu.ph>
2023-03-30 17:17:17 +0800

    added login w/valid credentials test

--- cypress/e2e/flender.cy.js
+++ cypress/e2e/flender.cy.js
@@ -2,8 +2,13 @@ const timeOut = {
     timeout: 1000000
 }
 
-beforeEach(function(){cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false',timeOut)})
+beforeEach(function () { cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false', timeOut) })
 
 describe('Create Protocol', function () {
-  
+    it('Login with Valid Credentials', function () {
+
+        cy.get('#username', timeOut).should('be.visible').type('t-sairel')
+        cy.get('#password', timeOut).should('be.visible').type('123')
+        cy.get('#login').should('be.visible').click()
+    })
 })
