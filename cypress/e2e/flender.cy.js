70162d9e26690d2abde09e0774c27477f392a84f
sairelcabahaga05 <scabahaga@ssct.edu.ph>
2023-03-30 17:14:28 +0800

    first commit

---
+++ cypress/e2e/flender.cy.js
@@ -0,0 +1,9 @@
+const timeOut = {
+    timeout: 1000000
+}
+
+beforeEach(function(){cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false',timeOut)})
+
+describe('Create Protocol', function () {
+  
+})
