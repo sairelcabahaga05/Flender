0767fc00e56df96bcf7796a8f40bd2c421d79157
sairelcabahaga05 <scabahaga@ssct.edu.ph>
2023-03-31 15:13:32 +0800

    added syntax to click a row and column

--- cypress/e2e/flender.cy.js
+++ cypress/e2e/flender.cy.js
@@ -11,4 +11,37 @@ describe('Create Protocol', function () {
         cy.get('#password', timeOut).should('be.visible').type('123')
         cy.get('#login').should('be.visible').click()
     })
+
+    it('Login with Invalid Credentials', function () {
+
+        cy.get('#username', timeOut).should('be.visible').type('user123') 
+        cy.get('#password', timeOut).should('be.visible').type('00000')  
+        cy.get('#login').should('be.visible').click()
+        cy.get('.toast-message').should('contain', 'Access denied') 
+    })
+
+    it.only('Create Protocol', function () {
+
+        cy.get('#username', timeOut).should('be.visible').type('t-sairel')
+        cy.get('#password', timeOut).should('be.visible').type('123')
+        cy.get('#login').should('be.visible').click()
+
+        cy.wait(80000)
+        cy.get('.scrollable > .protocols', timeOut).should('be.visible').click() 
+        cy.contains('Create protocol', timeOut).should('be.visible').click() 
+
+        cy.wait(5000)
+        cy.get('.button-container.left').first().within(() => {
+        cy.get('button').click()
+        cy.contains('FLENDER').click()
+        })
+
+        cy.wait(5000)
+        cy.get('.button-container.right').first().within(() => {
+        cy.get('button').click()
+        cy.contains('Voerde').click()
+        })
+    
+        cy.contains('Ok', timeOut).should('be.visible').click()
+    })
 })
